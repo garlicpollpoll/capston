41d131b69432d7f1edfabe76a31b22613e33459b
garlicpollpoll <kyoungsuk3254@naver.com>
2023-06-09 18:43:35 +0900

    application update commit

--- src/main/java/com/hello/capston/elasticsearch/repository/ElasticItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hello.capston.elasticsearch.repository;
-
-import com.hello.capston.elasticsearch.entity.ElasticItem;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ElasticItemRepository extends ElasticsearchRepository<ElasticItem, String> {
-
-
-}
