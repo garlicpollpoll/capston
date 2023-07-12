571be70a193408854fb49f3a492ebcd5473ccc62
garlicpollpoll <kyoungsuk3254@naver.com>
2023-07-12 18:50:47 +0900

    application update commit

--- src/main/java/com/hello/capston/controller/login/LogoutController.java
+++ src/main/java/com/hello/capston/controller/login/LogoutController.java
@@ -1,5 +1,6 @@
 package com.hello.capston.controller.login;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
+@Slf4j
 public class LogoutController {
 
     @GetMapping("/custom/logout")
@@ -18,17 +20,21 @@ public class LogoutController {
         HttpSession session = request.getSession(false);
         Cookie cookie = new Cookie("AUTH-TOKEN", null);
         Cookie sessionCookie = new Cookie("SESSION-TOKEN", null);
+        log.info("cookie setting");
 
         cookie.setMaxAge(0);
         cookie.setPath("/");
+        log.info("set cookie path");
 
         sessionCookie.setMaxAge(0);
         sessionCookie.setPath("/");
+        log.info("set cookie path");
 
         response.addCookie(cookie);
         response.addCookie(sessionCookie);
         session.removeAttribute("loginId");
         session.removeAttribute("userEmail");
+        log.info("remove cookie and remove session");
         return "redirect:/";
     }
 }
